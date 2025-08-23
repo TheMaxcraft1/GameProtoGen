b09e05bd9f3f4ab61d084049b8cd7ff4d7b724e4
Máximo Fain <98620343+TheMaxcraft1@users.noreply.github.com>
2025-08-23 17:31:43 -0300

    #1 Upload visual studio project

---
+++ GameProtoGen/GameProtoGen.cpp
@@ -0,0 +1,48 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <windows.h>
+
+#include <imgui.h>
+#include <imgui-SFML.h>   // 👈 backend ImGui para SFML
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        while (const auto event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
+
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+}
+
+// TODO: Uncomment the following lines to use WinMain instead of main. This makes the application a Windows GUI app instead of a console app.
+//int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+//    return main();
+//}
